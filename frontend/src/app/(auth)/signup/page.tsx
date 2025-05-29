ce8a306554c61ea2ce7d0b2853928407fdaef54c
GFA03 <florinalexandru302003@gmail.com>
2025-05-29 22:31:13 +0300

    fix: redirect logged-in user to '/events'

--- frontend/src/app/(auth)/signup/page.tsx
+++ frontend/src/app/(auth)/signup/page.tsx
@@ -1,7 +1,21 @@
+'use client';
+
 import {SignupForm} from "@/components/auth/SignupForm";
 import Link from "next/link";
+import { useAuth } from '@/contexts/AuthContext';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function SignupPage() {
+  const { isAuthenticated } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      router.push('/events');
+    }
+  }, [isAuthenticated, router]);
+
     return (
         <div className="min-h-[calc(100vh-150px)] flex items-center justify-center">
             <div className="max-w-md w-full bg-cyan-600 p-8 rounded-lg shadow-lg">
