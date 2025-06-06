81f7c6dfa26b6eb930bd5c1761c25f49bf7890f6
Gavrila Alexandru <florinalexandru302003@gmail.com>
2025-06-06 15:26:32 +0300

    fix: dtos bugs

--- backend/src/users/dto/user.dto.ts
+++ backend/src/users/dto/user.dto.ts
@@ -1,48 +1,9 @@
 import { Role } from '../entities/role.enum';
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsEnum, IsNotEmpty, IsString } from 'class-validator';
+import { IsEnum } from 'class-validator';
+import { CreateUserDto } from './create-user.dto';
 
-export class UserDto {
-  @ApiProperty({
-    name: 'email',
-    description: 'The user email',
-    type: String,
-    example: '<EMAIL>',
-  })
-  @IsNotEmpty()
-  @IsEmail()
-  readonly email: string;
-
-  @ApiProperty({
-    name: 'firstName',
-    description: 'The user first name',
-    type: String,
-    example: 'John',
-  })
-  @IsNotEmpty()
-  @IsString()
-  readonly firstName: string;
-
-  @ApiProperty({
-    name: 'lastName',
-    description: 'The user last name',
-    type: String,
-    example: 'Doe',
-  })
-  @IsNotEmpty()
-  @IsString()
-  readonly lastName: string;
-
-  @ApiProperty({
-    name: 'phoneNumber',
-    description: 'The user phone number',
-    type: String,
-    example: '0712345678',
-  })
-  @IsNotEmpty()
-  @IsString()
-  readonly phoneNumber: string;
-
+export class UserDto extends CreateUserDto {
   @ApiProperty({ enum: Role, default: Role.USER })
   @IsEnum(Role)
   readonly role: Role;
