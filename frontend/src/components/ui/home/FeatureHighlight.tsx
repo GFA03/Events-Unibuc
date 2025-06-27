e6596cee76639b1d1e6907f7ebaae233cabe93c0
Gavrila Alexandru <florinalexandru302003@gmail.com>
2025-06-27 18:24:45 +0300

    fix: empty classname

--- frontend/src/components/ui/home/FeatureHighlight.tsx
+++ frontend/src/components/ui/home/FeatureHighlight.tsx
@@ -19,7 +19,7 @@ export default function FeatureHighlight({
     <div
       className={`flex flex-col ${reverse ? 'lg:flex-row-reverse' : 'lg:flex-row'} items-center justify-around bg-blue-400 gap-8`}>
       <Image src={imageSrc} alt={alt} width={600} height={400} className="rounded-xl shadow-lg" />
-      <div className="">
+      <div>
         <h3 className="text-2xl text-white font-semibold mb-4">{title}</h3>
         <ul className="list-disc list-inside text-gray-200 space-y-2">
           {items.map((item, index) => (
