f84dd266e7a3b4b1b0c447c0842130f7e7f48484
Gavrila Alexandru <florinalexandru302003@gmail.com>
2025-07-02 21:49:31 +0300

    refactor: home page

--- frontend/src/app/(home)/page.tsx
+++ frontend/src/app/(home)/page.tsx
@@ -1,23 +1,15 @@
 'use client';
 
-import { useEvents } from '@/features/event/hooks/useEvents';
-import WithLoader from '@/components/ui/common/WithLoader';
 import HeroSection from '@/components/ui/home/HeroSection';
 import FeaturedEvents from '@/components/ui/home/FeaturedEvents';
 import HighlightsPresentation from '@/components/ui/home/HighlightsPresentation';
 
 export default function Home() {
-  const { data, isLoading, isError } = useEvents();
-
-  const events = data?.events || [];
-
   return (
-    <WithLoader isLoading={isLoading} isError={isError}>
-      <main className="w-full">
-        <HeroSection />
-        <FeaturedEvents events={events} />
-        <HighlightsPresentation />
-      </main>
-    </WithLoader>
+    <main className="w-full">
+      <HeroSection />
+      <FeaturedEvents />
+      <HighlightsPresentation />
+    </main>
   );
 }
--- frontend/src/components/ui/home/FeaturedEvents.tsx
+++ frontend/src/components/ui/home/FeaturedEvents.tsx
@@ -1,17 +1,15 @@
 import EventCard from '@/components/events/EventCard';
-import { Event } from '@/features/event/model';
 import Link from 'next/link';
+import { useEvents } from '@/features/event/hooks/useEvents';
+import { Suspense } from 'react';
+import LoadingSpinner from '@/components/ui/common/LoadingSpinner';
 
-export default function FeaturedEvents({ events }: { events: Event[] }) {
+export default function FeaturedEvents() {
   return (
     <section className="min-h-screen w-full bg-orange-50 py-16 px-6">
       <div className="max-w-7xl mx-auto">
         <h2 className="text-3xl font-bold text-black mb-8">Upcoming events</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {events.slice(0, 6).map((event) => (
-            <EventCard key={event.id} event={event} viewMode="grid" />
-          ))}
-        </div>
+        <EventLoader />
         <div className="text-right">
           <Link
             href="/events"
@@ -21,5 +19,21 @@ export default function FeaturedEvents({ events }: { events: Event[] }) {
         </div>
       </div>
     </section>
+  );
+}
+
+function EventLoader() {
+  const { data } = useEvents();
+
+  const events = data?.events || [];
+
+  return (
+    <Suspense fallback={<LoadingSpinner />}>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {events.slice(0, 6).map((event) => (
+          <EventCard key={event.id} event={event} viewMode="grid" />
+        ))}
+      </div>
+    </Suspense>
   );
 }
